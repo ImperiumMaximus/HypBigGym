dfe2d12fa9b54ae37a5b67f2b9bf538191473090
Raffaele Fioratto <raffaele.fioratto@gmail.com>
2015-06-08 19:38:00 +0200

    Cookie law friendly, some fixes too

--- js/all_course_cats.js
+++ js/all_course_cats.js
@@ -1,7 +1,7 @@
 
 jQuery(document).ready(function () {
     "use strict";
-    var elementHtml = "<div class=\"float-left\"><div class=\"tile-wide fg-black\"><div class=\"tile-content zooming\"><div class=\"slide\"><img class=\"cat-img\"/></div><div class=\"cat-title tile-label white-border\"></div></div></div><div class=\"margin5\"><center><a class=\"fg-white\" href=\"#\">Courses of this category</a></center></div></div>"
+    var elementHtml = "<div class=\"float-left\"><div class=\"tile-wide fg-black\"><div class=\"tile-content zooming\"><div class=\"slide\"><img class=\"cat-img\"/></div><div class=\"cat-title tile-label white-border\"></div></div></div><div class=\"margin5\"><center><a class=\"fg-white cat-list-link\" href=\"#\">Courses of this category</a></center></div></div>"
     console.log("here");
     jQuery.ajax({
         url: 'http://hypbiggym.altervista.org/php/getCourseCategories.php',
@@ -12,6 +12,7 @@ jQuery(document).ready(function () {
                 var elCat = jQuery(elementHtml);
                 elCat.find(".cat-title").html("<b>" + result[i].name + "</b>");
                 elCat.find(".cat-img").attr("src", result[i].main_photo);
+                elCat.find(".cat-list-link").attr("href", result[i].list_link);
                 jQuery("#content").append(elCat);
             }
         }
---
+++ js/cookie.js
@@ -0,0 +1,8 @@
+var _iub = _iub || [];
+_iub.csConfiguration = {
+    siteId: 313716, cookiePolicyId: 477402, lang: 'it'
+};
+(function (w, d) {
+    var loader = function () { var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src = "//cdn.iubenda.com/cookie_solution/iubenda_cs.js"; tag.parentNode.insertBefore(s, tag); };
+    if (w.addEventListener) { w.addEventListener("load", loader, false); } else if (w.attachEvent) { w.attachEvent("onload", loader); } else { w.onload = loader; }
+})(window, document);
