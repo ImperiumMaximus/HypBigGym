3fc2ebe820fd1de4ed3fbb7211348055bca7b57d
TheBestNick <nicolo.longoni@gmail.com>
2015-06-10 20:46:07 +0200

    staff_1
    Non funziona ancora il json

---
+++ js/staff_1.js
@@ -0,0 +1,18 @@
+jQuery(document).ready(function () {
+    "use strict";     
+    jQuery.ajax({
+        url: 'http://hypbiggym.altervista.org/php/getInstructorOfCourse.php',
+        data: {id: course},
+        success: function (data) {
+            console.log(data);
+            var result = JSON.parse(data);
+            var instructors = "";
+            for(var i=0; i<result.length;i++) {
+                instructors += "<a href ='" + result[i].link_instr + "'><p>" + result[i].name + " " + result[i].surname + "</p></a>";
+            }
+            
+            jQuery("#instructors").append(instructors);
+            
+        }
+    });
+});
---
+++ php/getInstructorOfCourse.php
@@ -0,0 +1,27 @@
+<?php
+require_once("db_config.php");
+
+$mysqli = new mysqli($hostname, $username, $password, $database);
+
+if ($mysqli->connect_error) {
+    die("Connect failed: " . $mysqli->connect_error);
+}
+
+$id = $mysqli->real_escape_string($_GET['id']);
+
+$queryInstructor = "SELECT `instructors`.`name`,`instructors`.`surname`,`instructors`.`link_instr` FROM `instructors` INNER JOIN `teaches` ON `instructors`.`id` = `teaches`.`instr_id` WHERE `teaches`.`course_name` = $id";
+$instructor = $mysqli->query($queryInstructor);
+
+$rows = array();
+
+while (($row = $instructor->fetch_assoc())) {
+    $rows[] = $row;
+                        
+}
+echo json_encode($row);
+
+$instructor->close();
+
+$mysqli->close();
+
+?>
