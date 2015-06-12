6f8cf5037aa4cc30d4b2b188fb32b63118aec411
TheBestNick <nicolo.longoni@gmail.com>
2015-06-13 01:36:52 +0200

    Sistemato il problema del sincro

---
+++ js/enrollment.js
@@ -0,0 +1,43 @@
+jQuery(document).ready(function () {
+    "use strict";     
+    jQuery.ajax({
+        url: 'http://hypbiggym.altervista.org/php/enrollment.php',
+        data: {id: course},
+        success: function (data) {
+            console.log(data);
+            var result = JSON.parse(data);
+            var date = "";
+            for(var i=0; i<result.length;i++) {
+                date += "<option value = '" + i + "'>" + result[i].day + " " + result[i].start_time + "</option>";
+            }
+            
+            jQuery("#criteria1").append(date);
+            
+        }
+    });
+});
+
+
+function scelta() {
+    console.log(jQuery("#criteria1").val());
+    var decisione = jQuery("#criteria1").val();
+    console.log(decisione);
+    
+    jQuery("#criteria2").empty();
+    
+    jQuery.ajax({
+        url: 'http://hypbiggym.altervista.org/php/enrollment.php',
+        data: {id: course},
+        success: function (data) {
+            var result = JSON.parse(data);
+            result.splice(decisione, 1);
+            var date = "<option value='' selected>Select a second date (optional)</option>";
+            for(var j=0; j<result.length;j++) {
+                date += "<option value = '" + j + "'>" + result[j].day + " " + result[j].start_time + "</option>";
+            }
+            
+            jQuery("#criteria2").append(date);
+            
+        }
+    });
+}
