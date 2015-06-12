350b1fa48d0c7bf7132ec6ecbdb02fc282ba92d3
TheBestNick <nicolo.longoni@gmail.com>
2015-06-13 01:39:28 +0200

    enrollment che sistemo

--- js/enrollment.js
+++ js/enrollment.js
@@ -21,7 +21,6 @@ jQuery(document).ready(function () {
 function scelta() {
     console.log(jQuery("#criteria1").val());
     var decisione = jQuery("#criteria1").val();
-    console.log(decisione);
     
     jQuery("#criteria2").empty();
     
@@ -29,10 +28,11 @@ function scelta() {
         url: 'http://hypbiggym.altervista.org/php/enrollment.php',
         data: {id: course},
         success: function (data) {
+            console.log(data);
             var result = JSON.parse(data);
-            result.splice(decisione, 1);
             var date = "<option value='' selected>Select a second date (optional)</option>";
-            for(var j=0; j<result.length;j++) {
+            for(var j=decisione; j<result.length;j++) {
+                j++;
                 date += "<option value = '" + j + "'>" + result[j].day + " " + result[j].start_time + "</option>";
             }
             
